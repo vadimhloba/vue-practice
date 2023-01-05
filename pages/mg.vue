ca642c569ae2236b4a00d89b5d92053e870127f3
Max Globa <onyx18121990@gmail.com>
2023-01-05 14:18:36 +0300

    added progress

--- pages/mg.vue
+++ pages/mg.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
 
+    <div class="progress">
+      <b>{{step * (100 / quiz.length)}}%</b>
+      <span :style="`width: ${step * (100 / quiz.length)}%;`"></span>
+    </div>
+
     <div v-if="quiz.length === step" class="thanks">
       <h1>Thank you, we will contact you shortly</h1>
       <p v-for="item in quiz">
@@ -10,7 +15,7 @@
     </div>
 
     <div v-else class="quiz">
-      <h1>{{ quiz[step].question }}</h1>
+      <h1 v-html="quiz[step].question"></h1>
       <div>
         <button
           v-for="(item, index) in quiz[step].options"
@@ -30,18 +35,23 @@ export default{
       step: 0,
       quiz: [
         {
-          question: 'How are you?',
-          options: ['good', 'nice', 'hyevo'],
+          question: 'Does your household participate in any of the following government programs?',
+          options: ['Yes', 'No'],
           answer: null
         },
         {
-          question: 'Where are you?',
-          options: ['usa', 'russia', 'china'],
+          question: 'Does your household currently have a Lifeline program phone?',
+          options: ['Not sure', 'Yes', 'No'],
           answer: null
         },
         {
-          question: 'What are you?',
-          options: ['men', 'women'],
+          question: 'Once you are qualified for the program, would you like a new phone for free?',
+          options: ['I want a free phone', 'I want to keep my smartphone (Receive a free Sim Card)', 'No'],
+          answer: null
+        },
+        {
+          question: `Does your current address match what the US Government has on file for your benefits? <br><br>(If you're not sure what address you have on file, you can call (800) 234-9473 to confirm)`,
+          options: ['Not sure', 'Yes', 'No'],
           answer: null
         }
       ]
@@ -67,3 +77,33 @@ export default{
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.progress{
+  height: 17.2px;
+  background: #93CEF6;
+  border-radius: 12.9323px;
+  width: 100%;
+  position: relative;
+  margin-top: 100px;
+  span{
+    position: absolute;
+    height: 26.28px;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    background: #005DC4;
+    border-radius: 12.9323px;
+    transition: width .3s ease;
+  }
+  b{
+    font-weight: 500;
+    font-size: 47.027px;
+    line-height: 57px;
+    color: #000000;
+    position: absolute;
+    left: 50%; top: -100%;
+    transform: translate(-100%, -100%);
+  }
+}
+</style>
