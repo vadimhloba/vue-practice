b03d6855e6172ef32f6fb92c36d5408358963f85
Yosuphew <vadimirua1409@gmail.com>
2023-01-06 14:51:43 +0200

    changed on scss styles

--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,17 @@
     </header>
 
     <div v-if="dowloadComplete" class="thanks">
-      <h1>Thank you, we will contact you shortly</h1>
-      <p v-for="item in quiz">
-        {{ item.questions }} - <b>{{ item.answer }}</b>
-      </p>
-      <button @click="reset">reset quiz</button>
+			<div class="container">
+	      <div class="wrapper">
+      		<h1>Thank you, we will contact you shortly</h1>
+					<div class="answers-text">
+      			<p v-for="item in quiz">
+      			  {{ item.question }} - <b>{{ item.answer }}</b>
+      			</p>
+					</div>
+      		<button @click="reset">reset quiz</button>
+				</div>
+			</div>
     </div>
 
     <section class="preload" v-if="quiz.length === step">
@@ -25,43 +31,41 @@
         <h3>{{ checkProgress }}</h3>
         <div class="dowloadPre">
           <div class="counter">{{ dowloadProgress }}%</div>
-          <Preloader class="preloader" :style="{ width: dowloadProgress + '%'}"/>
+          <Preloader class="preloader"/>
         </div>
       </div>
     </section>
 
     <section v-else class="main">
       <div class="container">
-        <div class="wrapper">
-	        <div class="wrap">
+        <div class="wrap"></div>
+	      <div class="wrapper">
 
-	          <h1>{{step * (100 / quiz.length)}}%</h1>
-
-	          <div class="slider">
-	            <div class="tab-move" :style="`width: ${step * (100 / quiz.length)}%;`"></div>
-	          </div>
-
-	          <div class="text-main">
-	            <h2 v-html="quiz[step].question"></h2>
-	            <ul v-if="quiz[step].list">
-	              <li v-for="item in quiz[step].list">{{ item }}</li>
-	            </ul>
-	          </div>
-
-	          <form v-if="step + 1 === quiz.length" @submit.prevent="answer(quiz[step].answer)">
-	            <input v-model="quiz[step].answer" type="text" placeholder="Enter Zip Code" required>
-	            <input type="submit" value="APPLY NOW">
-	          </form>
-
-	          <button
-	            v-for="(item, index) in quiz[step].options"
-	            @click="answer(item)"
-	            :key="`button_${index}`">
-	            {{ item }}
-	          </button>
-
+	        <h1>{{step * (100 / quiz.length)}}%</h1>
+	        <div class="slider">
+	          <div class="tab-move" :style="`width: ${step * (100 / quiz.length)}%;`"></div>
 	        </div>
-	       </div>
+
+	        <div class="text-main">
+	          <h2 v-html="quiz[step].question"></h2>
+	          <ul v-if="quiz[step].list">
+	            <li v-for="item in quiz[step].list">{{ item }}</li>
+	          </ul>
+	        </div>
+
+	        <form v-if="step + 1 === quiz.length" @submit.prevent="answer(quiz[step].answer)">
+	          <input v-model="quiz[step].answer" type="text" placeholder="Enter Zip Code" required>
+	          <input type="submit" value="APPLY NOW">
+	        </form>
+
+	        <button
+	          v-for="(item, index) in quiz[step].options"
+	          @click="answer(item)"
+	          :key="`button_${index}`">
+	          {{ item }}
+	        </button>
+
+	      </div>
       </div>
     </section>
   </div>
@@ -117,7 +121,7 @@ export default {
       	  if(this.dowloadComplete){
       	  	clearInterval(progressInterval)
       	  }
-      	}, 200);
+      	}, 300);
       }
     },
     reset(){
@@ -144,256 +148,291 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;800&display=swap');
 
-html, body{
+html, body {
   font-size: 14px;
   line-height: 1.2;
   margin: 0;
   padding: 0;
-  font-family: 'Montserrat', sans-serif;
+	height: auto;
+	font-family: 'Montserrat', sans-serif;
+	background: radial-gradient(131.56% 209.61% at 25.7% 172.94%, #F9FBFB 0%, #F9FBFB 0%, #3E81B8 100%, #204D8D 100%, #204D8D 100%), radial-gradient(59.31% 93.85% at 50% 57.19%, #F9FBFB 0%, #F9FBFB 0%, #3E81B8 100%, #204D8D 100%, #204D8D 100%), #FFFFFF;
 }
-.container{
+.container {
   width: 100%;
   margin-right: auto;
   margin-left: auto;
   padding: 0 15px;
 }
-@media (min-width: 576px){
+input[type=text], input[type=submit], button{
+	cursor: pointer;
+}
+button {
+	display: block;
+	width: 100%;
+	max-width: 670px;
+	margin: 0 auto;
+	height: 54px;
+	background: #A91313;
+	box-shadow: inset 0px 33px 23px rgba(255, 187, 187, 0.45);
+	border-radius: 15px;
+	border: 0;
+	font-weight: 700;
+	font-size: 18px;
+	line-height: 22px;
+	color: #FFFFFF;
+	margin-top: 30px;
+}
+@media (min-width: 576px) {
   .container {
     width: 540px;
   }
 }
-@media(min-width:768px){
-  .container{
+@media(min-width:768px) {
+  .container {
     width: 760px;
   }
 }
-@media(min-width:992px){
-  .container{
+@media(min-width:992px) {
+  .container {
     width: 990px;
   }
 }
-@media(min-width:1200px){
-  .container{
+@media(min-width:1200px) {
+  .container {
     width: 1190px;
   }
 }
+</style>
 
+<style lang="scss" scoped>
 /* #index START */
-.page-index{
-  background: radial-gradient(131.56% 209.61% at 25.7% 172.94%, #F9FBFB 0%, #F9FBFB 0%, #3E81B8 100%, #204D8D 100%, #204D8D 100%), radial-gradient(59.31% 93.85% at 50% 57.19%, #F9FBFB 0%, #F9FBFB 0%, #3E81B8 100%, #204D8D 100%, #204D8D 100%), #FFFFFF;
+.page-index {
+
+	/* header START */
+	header {
+	  padding: 21px 0 51px;
+		.container {
+	  	display: flex;
+	  	justify-content: flex-start;
+	  	align-items: center;
+		}
+		.logo-desc {
+	  	font-weight: 800;
+	  	font-size: 23.295px;
+	  	line-height: 28px;
+	  	text-transform: uppercase;
+	  	color: #FFFFFF;
+	  	width: 100%;
+	  	max-width: 130px;
+	  	margin-left: 12px;
+		}
+	}
+	/* header END */
+
+	/* preload START */
+	.preload {
+		position: relative;
+	  padding-top: 200px;
+	  padding-bottom: 265px;
+		.container {
+	  	justify-content: center;
+	  	display: block;
+		}
+		.dowloadPre {
+	  	position: relative;
+	  	justify-content: center;
+	  	align-items: center;
+	  	display: flex;
+	  	margin-top: 135px;
+		}
+		h3 {
+	  	position: absolute;
+	  	font-weight: 700;
+	  	font-size: 55px;
+	  	line-height: 67px;
+	  	color: #FFFFFF;
+			top: 10%;
+			left: 50%;
+			transform: translate(-50%, -10%);
+			max-width: 651px;
+			white-space: nowrap;
+		}
+		.counter {
+	  	position: absolute;
+	  	z-index: 2;
+	  	font-weight: 400;
+	  	font-size: 61.3081px;
+	  	line-height: 70px;
+	  	color: #000000;
+		}
+		.preloader {
+	  	display: block;
+	  	position: relative;
+	  	z-index: 1;
+	  	margin: 0 auto;
+	  	max-width: 311px;
+	  	width: 100%;
+		}
+	}
+	/* preload END */
+
+	/* section main START */
+	.main{
+	  position: relative;
+	  padding-bottom: 80px;
+		.container {
+	  	display: flex;
+	  	justify-content: center;
+	  	align-items: center;
+		}
+		.wrap {
+	  	position: relative;
+	  	max-width: 745px;
+	  	width: 100%;
+	  	height: 903px;
+	  	margin: 0 auto;
+	  	background: #FFFFFF;
+	  	border-radius: 14px;
+	  	padding: 5px 71px 5px 70px;
+	  	color: #000000;
+	  	z-index: 1;
+		}
+		.wrapper {
+	  	position: absolute;
+	  	z-index: 2;
+	  	height: auto;
+	  	max-width: 745px;
+	  	width: 100%;
+		}
+		h1 {
+	  	display: block;
+	  	width: 94px;
+	  	max-width: 100%;
+	  	font-weight: 500;
+	  	font-size: 47.027px;
+	  	line-height: 57px;
+	  	margin: 0 auto;
+		}
+		.slider {
+	  	position: relative;
+	  	margin: 16px 0 51px;
+	  	width: 100%;
+	  	height: 17.2px;
+	  	background: #93CEF6;
+	  	border-radius: 12.9323px;
+			.tab-move {
+	  		position: absolute;
+	  		height: 26.28px;
+	  		background: #005DC4;
+	  		border-radius: 12.9323px;
+	  		-webkit-border-radius: 12.9323px;
+	  		-moz-border-radius: 12.9323px;
+	  		transform: translateY(-50%);
+	  		transition: width .3s ease;
+	  		left: 0;
+	  		top: 50%;
+			}
+		}
+		.text-main {
+	  	padding: 0 61px 0 60px;
+		}
+		h2 {
+	  	font-weight: 800;
+	  	font-size: 24px;
+	  	line-height: 29px;
+	  	width: 100%;
+		}
+		ul {
+	  	margin-left: 28px;
+	  	padding: 31px 0 20px 10px;
+	  	width: 100%;
+	  	max-width: 541px;
+	  	list-style: outside;
+			li {
+	  		font-weight: 500;
+	  		font-size: 18px;
+	  		line-height: 126.9%;
+			}
+		}
+		input[type=text] {
+	  	display: block;
+	  	margin: 50px auto;
+	  	padding: 29px 0 28px 31px;
+	  	font-weight: 700;
+	  	font-size: 18px;
+	  	line-height: 22px;
+	  	color: #BFBFBF;
+	  	border: 1px solid #BFBFBF;
+	  	border-radius: 15px;
+	  	max-width: 670px;
+	  	width: 100%;
+	  	transition: .7s;
+			:focus {
+	  		border: 1px solid #555;
+			}
+		}
+		input[type=submit] {
+	  	display: block;
+	  	width: 100%;
+	  	max-width: 670px;
+	  	margin: 0 auto;
+	  	height: 54px;
+	  	background: #A91313;
+	  	box-shadow: inset 0px 33px 23px rgba(255, 187, 187, 0.45);
+	  	border-radius: 15px;
+	  	border: 0;
+	  	font-weight: 700;
+	  	font-size: 18px;
+	  	line-height: 22px;
+	  	color: #FFFFFF;
+	  	margin-top: 30px;
+		}
+	}
+	/* section main END */
+
+	/* div thanks START */
+	.thanks {
+		text-align: left;
+		.container {
+	  	display: flex;
+	  	justify-content: center;
+	  	align-items: center;
+		}
+		.wrapper {
+	  	max-width: 745px;
+	  	width: 100%;
+	  	height: auto;
+	  	margin: 100px auto 0;
+	  	background: #FFFFFF;
+	  	border-radius: 14px;
+	  	padding: 40px 70px;
+	  	color: #000000;
+	  	z-index: 1;
+		}
+		h1 {
+			text-align: center;
+			font-weight: 700;
+	  	font-size: 42px;
+	  	line-height: 57px;
+		}
+		.answers-text {
+			display: block;
+			max-width: 800px;
+			margin: 60px 0;
+			p {
+				font-weight: 500;
+	  		font-size: 17px;
+	  		line-height: 22px;
+			}
+		}
+		button {
+			font-size: 20px;
+		}
+	}
+	/* div thanks END */
 }
 /* #index END */
-
-/* header START */
-header{
-  padding: 21px 0 51px;
-}
-header .container{
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-header .logo-desc{
-  font-weight: 800;
-  font-size: 23.295px;
-  line-height: 28px;
-  text-transform: uppercase;
-  color: #FFFFFF;
-  width: 100%;
-  max-width: 130px;
-  margin-left: 12px;
-}
-/* header END */
-
-/* preload START */
-.preload{
-  padding-top: 200px;
-  padding-bottom: 265px;
-}
-.preload .container{
-  justify-content: center;
-  display: block;
-}
-.preload .dowloadPre{
-  position: relative;
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  margin-top: 135px;
-}
-.preload h3{
-  display: block;
-  font-weight: 700;
-  font-size: 55px;
-  line-height: 67px;
-  color: #FFFFFF;
-  margin: 0 auto;
-  max-width: 691px;
-  width: 100%;
-}
-.counter{
-  position: absolute;
-  z-index: 2;
-  font-weight: 400;
-  font-size: 61.3081px;
-  line-height: 70px;
-  color: #000000;
-}
-.preloader{
-  display: block;
-  position: relative;
-  z-index: 1;
-  margin: 0 auto;
-  max-width: 311px;
-  width: 100%;
-  animation: 6s ease-in-out infinite preloader-animation;
-}
-/* preload END */
-
-/* section main START */
-.main{
-  position: relative;
-  padding-bottom: 80px;
-}
-.main .container{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.main .wrapper{
-  position: relative;
-  max-width: 745px;
-  width: 100%;
-  height: 903px;
-  margin: 0 auto;
-  background: #FFFFFF;
-  border-radius: 14px;
-  padding: 5px 71px 5px 70px;
-  color: #000000;
-  z-index: 1;
-}
-.main .wrap{
-  position: absolute;
-  z-index: 2;
-  height: auto;
-  max-width: 745px;
-  width: 100%;
-}
-.main h1{
-  display: block;
-  width: 94px;
-  max-width: 100%;
-  font-weight: 500;
-  font-size: 47.027px;
-  line-height: 57px;
-  margin: 0 auto;
-}
-.main .slider{
-  position: relative;
-  margin: 16px 0 51px;
-  width: 100%;
-  height: 17.2px;
-  background: #93CEF6;
-  border-radius: 12.9323px;
-}
-.main .slider .tab-move{
-  position: absolute;
-  height: 26.28px;
-  background: #005DC4;
-  border-radius: 12.9323px;
-  -webkit-border-radius: 12.9323px;
-  -moz-border-radius: 12.9323px;
-  transform: translateY(-50%);
-  transition: width .3s ease;
-  left: 0;
-  top: 50%;
-}
-
-.main .text-main{
-  padding: 0 61px 0 60px;
-}
-.main h2{
-  font-weight: 800;
-  font-size: 24px;
-  line-height: 29px;
-  width: 100%;
-}
-.main ul{
-  margin-left: 28px;
-  padding: 31px 0 20px 10px;
-  width: 100%;
-  max-width: 541px;
-  list-style: outside;
-}
-.main li{
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 126.9%;
-}
-.main input[type=text]{
-  display: block;
-  margin: 50px auto;
-  padding: 29px 0 28px 31px;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 22px;
-  color: #BFBFBF;
-  border: 1px solid #BFBFBF;
-  border-radius: 15px;
-  max-width: 670px;
-  width: 100%;
-  transition: .7s;
-}
-input[type=text]:focus{
-  border: 1px solid #555;
-}
-.main input[type=submit]{
-  display: block;
-  cursor: pointer;
-  width: 100%;
-  max-width: 670px;
-  margin: 0 auto;
-  height: 54px;
-  background: #A91313;
-  box-shadow: inset 0px 33px 23px rgba(255, 187, 187, 0.45);
-  border-radius: 15px;
-  border: 0;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 22px;
-  color: #FFFFFF;
-  margin-top: 30px;
-}
-.main button{
-  display: block;
-  cursor: pointer;
-  width: 100%;
-  max-width: 670px;
-  margin: 0 auto;
-  height: 54px;
-  background: #A91313;
-  box-shadow: inset 0px 33px 23px rgba(255, 187, 187, 0.45);
-  border-radius: 15px;
-  border: 0;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 22px;
-  color: #FFFFFF;
-  margin-top: 30px;
-}
-/* section main END */
-
-/* key-frames START */
-@keyframes preloader-animation {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-/* key-frames END */
 </style>
